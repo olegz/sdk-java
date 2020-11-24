f58a0e496d78b434ae6ad2d3e5ef69b596471c5c
Dave Syer <dsyer@vmware.com>
2020-11-24 09:41:52 +0000

    Add test for POJO with webflux

--- spring/src/test/java/io/cloudevents/spring/webflux/WebFluxRestControllerTests.java
+++ spring/src/test/java/io/cloudevents/spring/webflux/WebFluxRestControllerTests.java
@@ -20,8 +20,7 @@ import java.util.UUID;
 
 import io.cloudevents.CloudEvent;
 import io.cloudevents.core.builder.CloudEventBuilder;
-import io.cloudevents.spring.webflux.CloudEventHttpMessageReader;
-import io.cloudevents.spring.webflux.CloudEventHttpMessageWriter;
+import io.cloudevents.spring.http.CloudEventHttpUtils;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 
@@ -41,6 +40,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.CodecConfigurer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -57,6 +57,32 @@ class WebFluxRestControllerTests {
 
 	@LocalServerPort
 	private int port;
+
+	@Test
+	void echoWithCorrectHeaders() {
+
+		ResponseEntity<String> response = rest.exchange(RequestEntity.post(URI.create("http://localhost:" + port + "/")) //
+				.header("ce-id", "12345") //
+				.header("ce-specversion", "1.0") //
+				.header("ce-type", "io.spring.event") //
+				.header("ce-source", "https://spring.io/events") //
+				.contentType(MediaType.APPLICATION_JSON) //
+				.body("{\"value\":\"Dave\"}"), String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isEqualTo("{\"value\":\"Dave\"}");
+
+		HttpHeaders headers = response.getHeaders();
+
+		assertThat(headers).containsKey("ce-id");
+		assertThat(headers).containsKey("ce-source");
+		assertThat(headers).containsKey("ce-type");
+
+		// assertThat(headers.getFirst("ce-id")).isNotEqualTo("12345");
+		assertThat(headers.getFirst("ce-type")).isEqualTo("io.spring.event.Foo");
+		assertThat(headers.getFirst("ce-source")).isEqualTo("https://spring.io/foos");
+
+	}
 
 	@Test
 	void requestResponseEvents() {
@@ -89,6 +115,14 @@ class WebFluxRestControllerTests {
 	@RestController
 	static class TestApplication {
 
+		@PostMapping("/")
+		public ResponseEntity<Foo> echo(@RequestBody Foo foo, @RequestHeader HttpHeaders headers) {
+			CloudEvent attributes = CloudEventHttpUtils.fromHttp(headers).withId(UUID.randomUUID().toString())
+					.withSource(URI.create("https://spring.io/foos")).withType("io.spring.event.Foo").build();
+			HttpHeaders outgoing = CloudEventHttpUtils.toHttp(attributes);
+			return ResponseEntity.ok().headers(outgoing).body(foo);
+		}
+
 		@PostMapping("/event")
 		public Mono<CloudEvent> event(@RequestBody Mono<CloudEvent> body) {
 			return body.map(event -> CloudEventBuilder.from(event).withId(UUID.randomUUID().toString())
@@ -107,6 +141,32 @@ class WebFluxRestControllerTests {
 
 		}
 
+	}
+
+}
+
+class Foo {
+
+	private String value;
+
+	public Foo() {
+	}
+
+	public Foo(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Foo [value=" + this.value + "]";
 	}
 
 }
